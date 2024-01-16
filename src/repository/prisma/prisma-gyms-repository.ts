3c675d9c830862b75b2e81b56cedc98a9e493197
enicio <jessandro42@gmail.com>
2024-01-16 00:43:57 -0300

    feat: add gyms repository

---
+++ src/repository/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,58 @@
+import { Prisma, Gym } from '@prisma/client'
+import { GymsRepository, getNearbyRequest } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+          {
+            description: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      },
+      skip: 20 * (page - 1),
+      take: 20,
+    })
+
+    return gyms
+  }
+
+  async getNearby(params: getNearbyRequest) {
+    const { latitude, longitude } = params
+    const gyms = await prisma.$queryRaw<Gym[]>`
+        SELECT * FROM gyms
+        WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+}
